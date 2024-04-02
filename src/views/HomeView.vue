a73f066cdbe633127b1d620a6719e0e4509fea07
Gabz047 <gabe1612mail@gmail.com>
2024-04-02 13:16:54 -0300

    Criação das páginas principais

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,51 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import { RouterLink } from 'vue-router';
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+ <header>
+    <div class="container-header">
+      <div class="container-logo">
+        <img src="../assets/minion.jpg" style="height: 90%; width: 90%; border-radius: inherit;">
+      </div>
+      <div class="container-links">
+      <RouterLink to="/veiculos">Veiculos</RouterLink>
+      <RouterLink to="/admin">Admin</RouterLink>
+      </div>
+    </div>
+ </header>
 </template>
+
+<style scoped>
+
+header {
+  width: 100%;
+  height: 15vh;
+}
+
+.container-header {
+  width: 100%;
+  height: 100%;
+  background-color: #191726;
+  display: flex;
+  justify-content: space-between;
+}
+
+
+.container-logo {
+  width: 10%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 50%;
+}
+
+.container-links {
+  width: 30%;
+  height: 100%;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+}
+</style>
