ee1dc30501cfb8d12c813d5cd4eceec582989287
Gabz047 <gabe1612mail@gmail.com>
2024-04-03 23:10:28 -0300

    Implementação do axios

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,32 @@
 <script setup>
 import { RouterLink } from 'vue-router';
-import  HeaderComp  from '../components/icons/HeaderComp.vue'
+import {ref, reactive, onMounted} from 'vue';
+import HeaderComp from '@/components/icons/HeaderComp.vue';
+import GeralApi from '@/api/geral'
+
+const geralApi = new GeralApi()
+const defaultCategoria = {id: null, descricao: ""}
+const categorias = ref([])
+const categoria = reactive({ ...defaultCategoria })
+
+onMounted(async () => {
+  categorias.value = await geralApi.buscarTodosOsDados()
+})
+
+function limpar() {
+  Object.assign(categoria, {...defaultCategoria})
+}
+
+
 </script>
 
 <template>
   <main>
     <HeaderComp />
+
+    <ul>
+      <li v-for="categoria in categorias" :key="categoria.id"> {{ categoria.descricao }} </li>
+    </ul>
 
     <div class="footer">
       <h4>Todos os direitos reservados | Fabrica IFC</h4>
